50ea621d5107b9c2a8b55ba5321b0402a7180cc5
Ubuntu <ubuntu@ip-172-31-11-219.us-east-2.compute.internal>
2020-02-25 19:10:36 +0000

    moved jupyter notebook code into a python script

--- data/get_data.py
+++ data/get_data.py
@@ -17,7 +17,7 @@ import subprocess
 def get_download_links():
     """Uses BeautifulSoup to parse "https://files.pushshift.io/reddit/comments/", which has all reddit comments by month. 
     Gets download links to the data and returns in a pandas dataframe"""
-    
+
     url_source = 'https://files.pushshift.io/reddit/comments/'
 
     source = urllib.request.urlopen(url_source).read()
@@ -31,21 +31,21 @@ def get_download_links():
             dot = rel_url.find('.')
             url_dict['month'].append(rel_url[3:dot])
             url_dict['link'].append(url_source + rel_url)
-    
+
     links_df = pd.DataFrame(url_dict)
     links_df['downloaded'] = False
     links_df['size_in_bytes'] = 0
     links_df['comment_count'] = 0
-    
+
     return links_df
 
 def download_file(download_url, s3_bucket = None, remove_file=True):
     """For a given file, this downloads the data and calls extract_file"""
 
     filename = download_url[download_url.rfind('/') + 1:]
-    filepath = './comment_files/' + filename
+    filepath = './data/comment_files/' + filename
     print('Downloading file...')
-    
+
     if s3_bucket is None:
         urllib.request.urlretrieve(download_url, filepath)
     else:
@@ -82,7 +82,7 @@ def extract_file(fp, remove_file):
 def mongo_import(month, fp):
     """Takes .json file and imports into a mongo database. Used mongoimport command because it's faster
     than pymongo. Used subprocess to run bash command within python.
-    
+
     Returns mongo database info"""
 
     db_name = 'reddit'
@@ -90,12 +90,12 @@ def mongo_import(month, fp):
     cmd = ['mongoimport', '-d', db_name, '-c', collection_name, '--file', fp]
     print('Loading to mongodb...')
     subprocess.run(cmd, check=True, text=True)
-    
+
     try:
         os.remove(fp)
     except:
         print("Error while deleting file ", fp)
-    
+
     return {'db_name': db_name, 'collection_name': collection_name, 'filepath': fp, 'month': month}
 
 def filter_comments(mongoinfo):
@@ -114,10 +114,10 @@ def filter_comments(mongoinfo):
                                 {'$eq': ['$link_id', '$parent_id']}}, 
                            {'_id': 1, 'author': 1, 'body': 1, 'created_utc': 1, 'id': 1, 'link_id': 1, 
                                 'parent_id': 1, 'score': 1, 'subreddit': 1})
-    
+
     my_db = client['myreddit']
     my_comments = my_db[collection_name]
-    
+
     print('Saving relevant comments...')
     for doc in cursor:
         my_comments.insert_one(doc)
@@ -154,7 +154,7 @@ def get_posts(praw_reddit, month):
              'sub': submission.subreddit.display_name, 
              'permalink': submission.permalink}
         posts.insert_one(d)
-    
+
     client.close()
 
 def main(praw_reddit, links_df=None, df_slice=None):
@@ -179,11 +179,11 @@ def main(praw_reddit, links_df=None, df_slice=None):
 
         comment_count = filter_comments(mongoinfo)
     #     get_posts(reddit, month)
-        
+
         links_df.loc[idx, 'downloaded'] = True
         links_df.loc[idx, 'size_in_bytes'] = filesize
         links_df.loc[idx, 'comment_count'] = comment_count
-        links_df.to_csv('comment_files/links_dataframe.csv', index=False)
+        links_df.to_csv('data/comment_files/links_dataframe.csv', index=False)
 
         print('Done! Time elapsed: {:1.2f}'.format(time.time() - start))
         print('')
@@ -197,6 +197,5 @@ if __name__ == "__main__":
                         user_agent='script:my.project:v1.0.0 (by /u/Someone')
 
     links_df = get_download_links()
-    links_df = links_df.iloc[28:50]
-    
-    main(reddit, links_df=links_df, df_slice=slice(50, 75))
+
+    main(reddit, links_df=links_df, df_slice=slice(51, 75))
